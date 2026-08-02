766cc2555ce91d7cca2b32b143cbbca3fcc28a70
subasriselvam03 <subasriselvam04@gmail.com>
2026-08-02 10:01:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ app.get("/test",function(req,res){
                 }
                 );
         });
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT}`);
 });
